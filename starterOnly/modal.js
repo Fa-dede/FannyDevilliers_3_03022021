50e0128bb29d64d2be307445f15a8930af0dfe06
Fa Dede <fanny.devilliers@gmail.com>
2021-02-03 19:00:33 +0100

    js: message de confirmation (non opérant)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,6 +8,7 @@ function editNav() {
 }
 
 // DOM Elements
+const modalForm = document.getElementById('modal-form');
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
@@ -37,20 +38,64 @@ let errorFirstName = document.getElementById('errorFirstName');
 let lastName = document.forms['reserve'] ['last'];
 let errorLasstName = document.getElementById('errorLastName');
 
+let email = document.forms['reserve'] ['email'];
+let errorEmail = document.getElementById('errorEmail');
 
+let birthDate = document.forms['reserve'] ['birthdate'];
+let errorBirthdate = document.getElementById('errorBirthdate');
+
+let radioButtons = document.forms['reserve'] ['location'];
+let errorRadioButtons = document.getElementById('errorRadioButtons');
+
+let cgv = document.forms['reserve'] ['checkbox1'];
+let errorCgv = document.getElementById('errorCgv');
+
+let buttonSubmit = document.getElementById('btn-envoi');
+let confirmMessage = document.getElementById('confirmation-popup');
 
 function validate() {
     if(firstName.value < 2 ){
-      firstName.style.backgroundColor = 'red';
+      firstName.style.backgroundColor = '#279e7a';
       errorFirstName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
       firstName.focus();
       return false;
+      
     }
-    if (lastName.value < 2 ){
-      lastName.style.backgroundColor = 'red';
+    else if (lastName.value < 2 ){
+      lastName.style.backgroundColor = '#279e7a';
       errorLastName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
       lastName.focus();
       return false;
     }
+    else if (email.value == ""){
+      email.style.backgroundColor = '#279e7a';
+      errorEmail.innerHTML = "Veuillez saisir votre adresse mail";
+      email.focus();
+      return false;
+    }
+    else if(birthDate.value == "") {
+      birthDate.style.backgroundColor = '#279e7a';
+      errorBirthdate.innerHTML = "Veuillez saisir votre date de naissance";
+      email.focus();
+      return false;
+    }
+
+    else if (cgv.checked == false) {
+      errorCgv.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+      return false;
+    }
 }
+
+// Message de confirmation d'inscription
+
+let showMessage = () => {
+  confirmMessage.style.display = "block";
+}
+
+buttonSubmit.addEventListener('click', showMessage);
+
+
+
+
+
 
